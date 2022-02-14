ddd7f93ba60be0c02c6d8b049626b08fdc5ec4d4
cheamology <calebcheam@gmail.com>
2022-02-14 15:16:08 +0800

    yo

---
+++ lib/events/event_repository.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_app/utils/sportevent.dart';
+
+class EventRepository {
+  // 1
+  final CollectionReference collection =
+      FirebaseFirestore.instance.collection('events');
+  // 2
+  Stream<QuerySnapshot> getStream() {
+    return collection.snapshots();
+  }
+
+  // 3
+  Future<DocumentReference> addEvent(SportEvent e) {
+    return collection.add(e.toJson());
+  }
+
+  // 4
+  void updateEvent(SportEvent e, String key) async {
+    await collection.doc(key).update(e.toJson());
+  }
+
+  // 5
+  void deleteEvent(SportEvent e, String key) async {
+    await collection.doc(key).delete();
+  }
+}
