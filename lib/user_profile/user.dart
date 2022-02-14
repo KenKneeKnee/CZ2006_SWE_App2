88dfead3d3e122ca7db3cbbe5ef4c1cae941ebfa
cheamology <calebcheam@gmail.com>
2022-02-14 16:28:42 +0800

    user data

--- lib/user_profile/user.dart
+++ lib/user_profile/user.dart
@@ -1,16 +1,33 @@
-class User {
-  final String userid;
-  final String username;
+class UserData {
+  late final String userid;
+  late final String username;
   late final int points;
   late final List<String> friend;
   late final List<String> friendrequests;
   late final int reports;
 
-  User(this.userid, this.username) {
+  UserData(this.userid, this.username) {
     this.points = 0;
     this.reports = 0;
     friend = List.empty();
     friendrequests = List.empty();
+  }
+
+  String get_userid() {
+    return userid;
+  }
+
+  int get_points() {
+    return points;
+  }
+
+  String get_username() {
+    return username;
+  }
+
+  // ignore: non_constant_identifier_names
+  set_points(int newpoints) {
+    this.points = newpoints;
   }
 }
 
