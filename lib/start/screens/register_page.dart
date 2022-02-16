1e9c5b4db6fc701bd0177d135aa9b5bc0400d769
cheamology <calebcheam@gmail.com>
2022-02-16 12:32:52 +0800

    pushing this so clarissa can push without conflict

--- lib/start/screens/register_page.dart
+++ lib/start/screens/register_page.dart
@@ -145,15 +145,6 @@ class _RegisterPageState extends State<RegisterPage> {
                                   password: _passwordTextController.text,
                                 );
 
-                                await userdb.add(new UserData(
-                                        _emailTextController.text,
-                                        _nameTextController.text,
-                                        0,
-                                        0,
-                                        List<dynamic>.empty(),
-                                        List<dynamic>.empty())
-                                    .toJson());
-
                                 setState(() {
                                   _isProcessing = false;
                                 });
