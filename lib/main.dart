5e31769dbc376e10afb6401ee37e4ba847b88e5b
Joel <nyjc39sc.joel@gmail.com>
2022-02-23 10:01:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:my_app/calendar/grrrrrrr.dart';
+import 'package:my_app/events/view_event.dart';
 import 'package:my_app/map/facil_map.dart';
 import 'package:my_app/start/screens/login_page.dart';
 import 'package:my_app/start/screens/register_page.dart';
