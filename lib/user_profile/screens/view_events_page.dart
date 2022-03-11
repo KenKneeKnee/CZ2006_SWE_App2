e6def62767a89166c376444cd0153349f52818a9
cheamology <calebcheam@gmail.com>
2022-03-11 22:27:08 +0800

    user profile 1.5

--- lib/user_profile/screens/view_events_page.dart
+++ lib/user_profile/screens/view_events_page.dart
@@ -18,81 +18,175 @@ class _bookingPageState extends State<bookingPage> {
   final EventRepository repository = EventRepository();
   final BookingRepository booking = BookingRepository();
 
+  ScrollController controller = ScrollController();
+  double topContainer = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      double value = controller.offset / 1000;
+
+      setState(() {
+        topContainer = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return StreamBuilder<QuerySnapshot>(
-      stream: booking.getStream(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot1) {
-        return StreamBuilder<QuerySnapshot>(
-          stream: repository.getStream(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return const Text('Something went wrong');
-            }
-            if (!snapshot.hasData) {
-              return const CircularProgressIndicator();
-            }
-            List EventList = snapshot.data!.docs;
-            List BookingList = snapshot1.data!.docs;
-            Map<String, SportEvent> PastEventMap = {};
-            Map<String, SportEvent> ActiveEventMap = {};
-            List activeEventIds = [];
-            List pastEventIds = [];
-            for (DocumentSnapshot doc in BookingList) {
-              if (doc['userId'] == uid) {
-                if (doc['active'] == true) {
-                  activeEventIds.add(doc['eventId']);
+        stream: booking.getStream(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot1) {
+          return StreamBuilder<QuerySnapshot>(
+              stream: repository.getStream(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text('Something went wrong');
                 }
-              } else {
-                pastEventIds.add(doc['eventId']);
-              }
-            }
-            for (String eid in pastEventIds) {
-              for (DocumentSnapshot doc in EventList) {
-                if (doc.id == eid) {
-                  SportEvent e = SportEvent.fromSnapshot(doc);
-                  PastEventMap[eid] = e;
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
                 }
-              }
-            }
+                List EventList = snapshot.data!.docs;
+                List BookingList = snapshot1.data!.docs;
+                Map<String, SportEvent> PastEventMap = {};
+                Map<String, SportEvent> ActiveEventMap = {};
+                List activeEventIds = [];
+                List pastEventIds = [];
+                final Size size = MediaQuery.of(context).size;
 
-            for (String eid in activeEventIds) {
-              for (DocumentSnapshot doc in EventList) {
-                if (doc.id == eid) {
-                  SportEvent e = SportEvent.fromSnapshot(doc);
-                  ActiveEventMap[eid] = e;
-                }
-              }
-            }
+                List<Widget> eventbuttons = [];
 
-            return Scaffold(
-                appBar: buildAppBar(context),
-                body: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: PastEventMap.length,
-                    padding: const EdgeInsets.only(top: 10.0),
-                    itemBuilder: (context, index) {
-                      String key = PastEventMap.keys.elementAt(index);
-                      SportEvent curEvent = PastEventMap[key] as SportEvent;
-                      return Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Row(children: <Widget>[
-                          Container(
-                            child: Expanded(
-                              child: Text(
-                                  'id: ${key} event: ${curEvent.name} at ${curEvent.placeId} curCap: ${curEvent.curCap} maxCap: ${curEvent.maxCap}'),
+                for (DocumentSnapshot doc in BookingList) {
+                  if (doc['userId'] == uid) {
+                    if (doc['active'] == true) {
+                      activeEventIds.add(doc['eventId']);
+                    }
+                  } else {
+                    pastEventIds.add(doc['eventId']);
+                  }
+                }
+
+                for (String eid in pastEventIds) {
+                  for (DocumentSnapshot doc in EventList) {
+                    if (doc.id == eid) {
+                      SportEvent e = SportEvent.fromSnapshot(doc);
+                      PastEventMap[eid] = e;
+                    }
+                  }
+                }
+
+                for (String eid in activeEventIds) {
+                  for (DocumentSnapshot doc in EventList) {
+                    if (doc.id == eid) {
+                      SportEvent e = SportEvent.fromSnapshot(doc);
+                      ActiveEventMap[eid] = e;
+                    }
+                  }
+                }
+
+                for (String eventid in activeEventIds) {
+                  SportEvent currentevent =
+                      ActiveEventMap[eventid] as SportEvent;
+
+                  eventbuttons.add(Container(
+                      height: size.height * 0.2,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withAlpha(100),
+                                blurRadius: 10.0),
+                          ]),
+                      child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      currentevent.name.toString(),
+                                      style: const TextStyle(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "Current capacity: " +
+                                          currentevent.curCap.toString() +
+                                          "/" +
+                                          currentevent.maxCap.toString(),
+                                      style: const TextStyle(
+                                          fontSize: 17, color: Colors.grey),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    FloatingActionButton.extended(
+                                        onPressed: () {},
+                                        label: const Text('View'),
+                                        backgroundColor: Colors.orange),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                  ],
+                                ),
+                              ]))));
+                }
+
+                return SafeArea(
+                    child: Scaffold(
+                        backgroundColor: Colors.white,
+                        appBar: AppBar(
+                          elevation: 0,
+                          backgroundColor: Color(0xffE3663E),
+                          leading: BackButton(
+                              color: Colors.black,
+                              onPressed: () => Navigator.of(context).pop()),
+                          actions: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.search, color: Colors.black),
+                              onPressed: () {},
                             ),
-                          ),
-                        ]),
-                      );
-                    }));
-          },
-        );
-      },
-    );
+                          ],
+                        ),
+                        body: Container(
+                            height: size.height,
+                            child: ListView.builder(
+                                controller: controller,
+                                itemCount: eventbuttons.length,
+                                physics: const BouncingScrollPhysics(),
+                                itemBuilder: (context, index) {
+                                  double scale = 1.0;
+                                  if (topContainer > 0.1) {
+                                    scale = index + 0.1 - topContainer;
+                                    if (scale < 0) {
+                                      scale = 0;
+                                    } else if (scale > 1) {
+                                      scale = 1;
+                                    }
+                                  }
+                                  return Opacity(
+                                    opacity: scale,
+                                    child: Transform(
+                                      transform: Matrix4.identity()
+                                        ..scale(scale, scale),
+                                      alignment: Alignment.bottomCenter,
+                                      child: Align(
+                                          heightFactor: 0.8,
+                                          alignment: Alignment.topCenter,
+                                          child: eventbuttons[index]),
+                                    ),
+                                  );
+                                }))));
+              });
+        });
   }
 }
