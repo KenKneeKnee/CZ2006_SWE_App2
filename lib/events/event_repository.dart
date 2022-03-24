f347056816822715720efa54bed8f143647ddae5
Joel <nyjc39sc.joel@gmail.com>
2022-03-24 10:24:03 +0800

    latest update

--- lib/events/event_repository.dart
+++ lib/events/event_repository.dart
@@ -26,12 +26,5 @@ class EventRepository {
     await collection.doc(key).delete();
   }
 
-  void completeEvent(String key) async {
-    collection.where("eventId", isEqualTo: key).get().then((value) {
-      value.docs.forEach((result) {
-        collection.doc(result.id).update({"active": false});
-      });
-    });
-  }
 
 }
