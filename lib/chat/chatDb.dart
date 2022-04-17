a0dc535f9d3e77246257ce62641138151e54fb4a
KenKneeKnee <khantnyinyi.11@gmail.com>
2022-04-17 18:14:38 +0800

    document chat

--- lib/chat/chatDb.dart
+++ lib/chat/chatDb.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/// Persistent data surrounding chat messages
 class DatabaseService {
   // Collection reference
   final CollectionReference EventCollection =
